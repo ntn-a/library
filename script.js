a443b59ea8d0b46ea6e4414e2c63798aded7e52b
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-08-16 12:42:09 -0700

    Added JS and div.

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 let myLibrary = [];
 
-function Book() {
-
+class Book {
+  constructor() {
+    this.title = "";
+    this.author = "";
+    this.pages = 0;
+    this.read = false;
+  }
+  setTitle(newTitle) {
+    this.title = newTitle; 
+  }
+  setAuthor(newAuthor) {
+    this.author = newAuthor;
+  }
+  setPages(newPages) {
+    this.pages = newPages;
+  }
+  setRead(newRead) {
+    this.read = newRead;
+  }
 }
 
-function addBookToLibrary() {
-  
+function addBookToLibrary(bookObject) {
+  myLibrary.push(bookObject);
+}
+
+function printLibrary() {
+  for (let i = 0; i < myLibrary.length; i++){
+    console.log(myLibrary[i]);
+  }
 }
