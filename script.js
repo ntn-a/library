e7d112330ffcd1e5c7459deb36e364f342eca645
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-08-16 21:05:05 -0700

    Added working title entry.

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 let myLibrary = [];
 const btn = document.querySelector(".add");
 const form = document.querySelector(".form");
-const exit = document.querySelector(".exit")
-
+const exit = document.querySelector(".exit");
+const submit = document.querySelector(".submit");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const pages = document.querySelector(".pages");
+const main = document.querySelector(".main")
+const book = document.querySelector(".book");
 
 btn.addEventListener("click", () => {
   form.classList.toggle("active");
 })
 
-
 exit.addEventListener("click", () => {
   form.classList.toggle("active");
 })
 
+submit.addEventListener("click", () => {
+  entry = new Book()
+  entry.setTitle(title.value);
+  entry.setAuthor(author.value);
+  entry.setPages(pages.value);
+  addBookToLibrary(entry);
+  for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
+  }
+  displayBooks();
+  form.classList.toggle("active");
+})
 
 class Book {
   constructor() {
@@ -33,15 +49,37 @@ class Book {
   setRead(newRead) {
     this.read = newRead;
   }
+  getTitle() {
+    return this.title;
+  }
+  getAuthor() {
+    return this.author
+  }
+  getPages() {
+    return this.pages;
+  }
+  getRead() {
+    return this.read;
+  }
 }
 
 function addBookToLibrary(bookObject) {
   myLibrary.push(bookObject);
 }
 
-function printLibrary() {
-  for (let i = 0; i < myLibrary.length; i++){
-    console.log(myLibrary[i]);
+function clearDisplay() {
+  var divsToRemove = document.querySelectorAll(".book");
+  for (let i = 0; i < divsToRemove.length; i++){
+    divsToRemove[i].remove();
   }
 }
 
+function displayBooks() {
+  clearDisplay();
+  for (var i = 0; i < myLibrary.length; i++) {
+    const content = document.createElement("div");
+    content.classList.add("book");
+    content.textContent = myLibrary[i].title;
+    main.appendChild(content);
+  }
+}
